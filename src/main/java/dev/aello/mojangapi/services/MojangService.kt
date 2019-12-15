e940f8088425c2f1e95bbbb8eedf9de7a98b84b5
Sandro Petrovski <admin@aello.dev>
2019-12-15 15:47:36 -0500

    Add functions to MojangService

--- src/main/java/dev/aello/mojangapi/services/MojangService.kt
+++ src/main/java/dev/aello/mojangapi/services/MojangService.kt
@@ -1,4 +1,21 @@
 package dev.aello.mojangapi.services
 
+import dev.aello.mojangapi.components.MojangStatistics
+import dev.aello.mojangapi.components.Player
+import retrofit2.Call
+import retrofit2.http.*
+import java.util.*
+
 interface MojangService {
+    @GET("users/profiles/minecraft/{username}?at={timestamp}")
+    fun getProfileAt(@Query("at") at: Long = System.currentTimeMillis() / 1000): Call<Player>
+
+    @GET("user/profiles/{uuid}/names")
+    fun getNameHistory(@Path("uuid") uuid: UUID): Call<List<Map<String, String>>>
+
+    @POST("profiles/minecraft")
+    fun getMultipleProfiles(@Body player: List<String>): Call<List<Player>>
+
+    @POST("orders/statistics")
+    fun getStatistics(@Body metricKeys: List<String> = listOf("item_sold_minecraft")): Call<MojangStatistics>
 }
